6d38b9e65ea90503e9b4f5f62b1cf650b0c6a8d1
Scott Prue <scott.prue@borderfree.com>
2015-09-14 17:18:30 -0400

    Build page now has Code Editor, File Structure, and device preview. Files structure is being loaded from Grout.

--- public/applications/application/build/build.controller.js
+++ public/applications/application/build/build.controller.js
@@ -1,9 +1,17 @@
 angular.module('tessellate.application.build')
-.controller('BuildCtrl', ['$scope', '$log','$q', '$grout', 'application', function ($scope, $log, $q, $grout, application){
+.controller('BuildCtrl', ['$scope', '$log','$q', '$sce', '$grout', 'application', function ($scope, $log, $q, $sce, $grout, application){
   //Copy application data into scope
   console.log('application loaded:', $scope.application);
   $scope.application = _.extend({},application);
-  $scope.structure = [{name: 'index.html', type:'file'}, {name: 'testfolder', type:'folder', children:[{name:'infolder.js', type:'file'}]}]
+  // $scope.structure = [{name: 'index.html', type:'file'}, {name: 'testfolder', type:'folder', children:[{name:'infolder.js', type:'file'}]}]
+  $scope.previewData = {device:'iphone5', url:$sce.trustAsResourceUrl("http://" + $scope.application.frontend.siteUrl)};
+  $grout.App(application.name).Files.getStructure.then(function (fileStructure){
+      $log.log('structure loaded:', fileStructure);
+      $scope.structure = fileStructure;
+      $scope.$apply();
+    }, function (err){
+      $log.error('Error getting file structure:', err);
+    });
   $scope.aceLoaded = function(_editor) {
     // Editor.setAce(_editor);
   };
