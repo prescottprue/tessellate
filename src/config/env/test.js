c4febc363e08098232b34758a5864ada636fe124
Scott Prue <prescottprue@gmail.com>
2016-04-08 04:20:45 -0400

    Firebase vars added test env of config

--- src/config/env/test.js
+++ src/config/env/test.js
@@ -5,20 +5,14 @@
  */
 
 module.exports = {
-  db: 'mongodb://localhost/noobjs_test',
-  github: {
-    clientID: '',
-    clientSecret: '',
-    callbackURL: 'http://localhost:3000/auth/github/callback'
-  },
-  google: {
-    clientID: 'asdfasdf',
-    clientSecret: 'asdfasdf',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
-  },
+  db: 'mongodb://localhost/tessellate_test',
   auth: {
     enabled: true,
     secret: 'shhhhh',
     cookieName: 'tessellate'
+  },
+  firebase: {
+    url: 'https://kyper-tech.firebaseio.com/tessellate',
+    secret: process.env.TESSELLATE_FIREBASE_SECRET || process.env.FIREBASE_SECRET || 'asdf'
   }
 }
